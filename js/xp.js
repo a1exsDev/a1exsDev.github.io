8e728f272b325f17a8d2d88152f814805e29674e
a1exsDev <alexsvaldez2011@gmail.com>
2022-11-20 00:39:20 -0600

    fixed things

--- js/xp.js
+++ js/xp.js
@@ -1,15 +1,17 @@
-setInterval(updateXp, 60000);
-setInterval(updateTimer, 1000);
-
+var docById = document.getElementById;
+var docByClass = document.getElementsByClassName;
 var secs = 0;
 var xp = 0;
 
 function updateTimer() {
     secs += 1;
-    document.getElementById("timer").innerHTML = secs;
-    document.getElementById("xp").innerHTML = xp;
+    docById("timer").innerHTML = secs;
+    docByClass("xp").innerHTML = xp;
 }
 
 function updateXp() {
     xp += 1;
 }
+
+setInterval(updateXp, 60000);
+setInterval(updateTimer, 1000);
