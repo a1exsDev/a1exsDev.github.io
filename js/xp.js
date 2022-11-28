3c8ec717dff13505c86b84e3a0cdd41eaa6a0704
a1exsDev <alexsvaldez2011@gmail.com>
2022-11-27 20:44:20 -0600

    fixed about and navbar

--- js/xp.js
+++ js/xp.js
@@ -1,15 +1,17 @@
-var docById = document.getElementById;
-var docByClass = document.getElementsByClassName;
-var secs = 0;
-var xp = 0;
+var docById = document.getElementById,
+    docByClass = document.getElementsByClassName,
+    secs = 0,
+    xp = 0;
 
 function updateTimer() {
+    "use strict";
     secs += 1;
     docById("timer").innerHTML = secs;
     docByClass("xp").innerHTML = xp;
 }
 
 function updateXp() {
+    "use strict";
     xp += 1;
 }
 
