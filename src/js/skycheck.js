75b8de992094ee0ff96ebc79756bdc278a7489d6
a1exsDev <alexsvaldez2011@gmail.com>
2023-01-16 19:23:14 -0600

    didn't do much

--- src/js/skycheck.js
+++ src/js/skycheck.js
@@ -1,20 +1,16 @@
-function getUserInfo() {    
+function getUserStats() {    
     // Redirect the user to the new URL with the username as a parameter
     const username = document.getElementById("username").value;
-    window.location.href = `https://a1exsDev.github.io/skycheck.html/${username}`;
 
-    // Extract the username from the URL
-    const url = new URL(window.location.href);
-    const usernameFromUrl = url.pathname.split("/").pop();
 
     // Use the extracted username to make the API request
     const apiEndpoint = "https://sky.shiiyu.moe/api/v2/profile/";
-    fetch(`${apiEndpoint}${usernameFromUrl}`)
+    fetch(`${apiEndpoint}${username}`)
         .then(response => response.json())
         .then(data => {
             // Show the data about the user
-            const output = document.getElementById("output");
-            output.innerHTML = JSON.stringify(data);
+            const content = document.getElementById("content");
+            content.innerHTML = JSON.stringify(data);
         })
         .catch(error => {
             console.error(error);
